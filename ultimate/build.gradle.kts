f25bb1f39393375c15df7bf4151549ae02eee34f
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-10-13 16:25:09 +0300

    Remove ultimate settings from AS branch

--- ultimate/build.gradle.kts
+++ ultimate/build.gradle.kts
@@ -1,3 +1,5 @@
+/*
+
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.jvm.tasks.Jar
@@ -168,3 +170,6 @@ projectTest {
 }
 
 val generateTests by generator("org.jetbrains.kotlin.tests.GenerateUltimateTestsKt")
+
+
+*/
