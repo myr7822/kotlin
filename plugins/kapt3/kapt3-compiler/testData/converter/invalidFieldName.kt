095435f365cf44a34e5a122c0808771ff3d10898
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-11-07 12:06:13 +0900

    Kapt: Add Kotlin location to diagnostics reported in Kapt stubs

--- plugins/kapt3/kapt3-compiler/testData/converter/invalidFieldName.kt
+++ plugins/kapt3/kapt3-compiler/testData/converter/invalidFieldName.kt
@@ -1,6 +1,6 @@
-// EXPECTED_ERROR 'WHI-TE' is an invalid Java enum value name
-// EXPECTED_ERROR an enum annotation value must be an enum constant
-// EXPECTED_ERROR cannot find symbol
+// EXPECTED_ERROR(-1;-1) 'WHI-TE' is an invalid Java enum value name
+// EXPECTED_ERROR(4;20) an enum annotation value must be an enum constant (Kotlin location: /invalidFieldName.kt: (9, 1))
+// EXPECTED_ERROR(4;20) cannot find symbol (Kotlin location: /invalidFieldName.kt: (9, 1))
 
 enum class Color {
     BLACK, `WHI-TE`
